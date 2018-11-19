193e5a82e604d86821b0c30406129be9136c4014
Dorian Tudorache <dorian.snaz@gmail.com>
2018-11-19 10:17:40 +0200

    ported jest & ts

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  verbose: true,
+  bail: true,
+  coverageReporters: ['html', 'text'],
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+      diagnostics: false
+    }
+  },
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'ts',
+    'tsx'
+  ],
+  moduleNameMapper: {
+    'lodger/(.*)$': '<rootDir>/$1',
+    '~/(.*)$': '<rootDir>/$1',
+    'forms/(.*)$': '<rootDir>/lib/forms/$1',
+    'lodger.config': '<rootDir>/lodger.config'
+  },
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  preset: 'ts-jest',
+  // transformIgnorePatterns: ['/node_modules/', '/lodger/'],
+  // testRegex: '(/__tests__/.*| (\\.| /)(test|spec))\\.(jsx?|tsx?)$'
+  testRegex: null
+}
