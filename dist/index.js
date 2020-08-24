bb96dbaa74fca94f66deeb0c243c902b01d854e6
Dorian Tudorache <dorian.snaz@gmail.com>
2020-08-24 12:31:56 +0300

    test 100

--- dist/index.js
+++ dist/index.js
@@ -3306,23 +3306,6 @@ var en$1 = /*#__PURE__*/Object.freeze({
     'default': en
 });
 
-module.exports = {
-  taxonomies: {
-    asociatie: {
-      name: 'Asociație',
-      plural: 'Asociații',
-      fields: {}
-    }
-  },
-  errors: {
-    index: {
-      missingDB: 'Bază de date nespecificată',
-      invalidPluginDefinition: 'Plugin invalid',
-      couldNotWriteFile: 'Fișierul %% nu poate fi scris'
-    }
-  }
-};
-
 var ro = /*#__PURE__*/Object.freeze({
     __proto__: null
 });
