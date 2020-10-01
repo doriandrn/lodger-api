10d30e22493a4bdb7ec36a0120b374af00a1be41
Dorian Tudorache <dorian.snaz@gmail.com>
2020-10-01 09:41:46 +0300

    test

--- rollup.config.js
+++ rollup.config.js
@@ -53,11 +53,11 @@ export default {
           case 'ratesAtCompileTime':
             let rates = {}
 
-            await axios.get('https://api.coingate.com/v2/rates/merchant').then(({ data }) => {
-              rates = data
+            await axios.get('https://api.coingate.com/v2/rates/trader').then(({ data }) => {
+              rates = data.buy
             })
 
-            console.log(`Updated ${Object.keys(rates).length} currency rates`, rates)
+            console.log(`Updated ${Object.keys(rates).length} currency rates`)
             fs.writeFileSync(path.join(__dirname, 'dist/ratesAtCompileTime.json'), JSON.stringify(rates))
             if (rates.RON) fs.writeFileSync(path.join(__dirname, 'dist/currencies.json'), JSON.stringify(Object.keys(rates.RON)))
             return `export default ${ JSON.stringify(rates) }`;
