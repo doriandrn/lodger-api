8d7cc20308b9486bc983cf45170ebde152d19de9
Dorian Tudorache <dorian.snaz@gmail.com>
2020-10-05 21:45:33 +0300

    test

--- src/lib/Taxonomy/Subscribable.ts
+++ src/lib/Taxonomy/Subscribable.ts
@@ -96,7 +96,7 @@ implements SubscribableTaxonomy<T> {
             const { selectedId } = taxSub
             if (!taxSub._refsIds) {
               taxSub._refsIds = observable.box({})
-              taxSub.refsIds = computed(taxSub._refsIds)
+              taxSub.refsIds = taxSub._refsIds.get()
             }
             if (selectedId) {
               taxSub._refsIds[plural === tax ? plural : `${tax}Id`] = plural === tax ? [ selectedId ] : selectedId
