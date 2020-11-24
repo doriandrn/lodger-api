ad2debf56e79eaf7202196e3038e4f2a0ffd4371
Dorian Tudorache <dorian.snaz@gmail.com>
2020-11-24 19:08:13 +0200

    try again

--- src/lib/Taxonomy/index.ts
+++ src/lib/Taxonomy/index.ts
@@ -164,7 +164,7 @@ export default class Taxonomy<T extends Taxonomie, Interface = { updatedAt ?: nu
     this.$collection = collection
   }
 
-  get collection {
+  get collection () {
     return this.$collection
   }
 
