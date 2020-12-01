8da0cf68944550ca0bf7f03fa74f70a10440fda7
Dorian Tudorache <dorian.snaz@gmail.com>
2020-12-01 11:47:31 +0200

    tax children fixes test 6

--- src/index.ts
+++ src/index.ts
@@ -286,7 +286,8 @@ class Lodger implements LodgerAPI {
 
         const childrenKeys = $t.form.fieldsIds
           .filter(key => [`${$tax.form.name}Id`, $tax.form.plural].indexOf(key) > -1 &&
-          key !== 'servicii' && parents.map(p => p.plural).indexOf(key.replace('Id', '').plural) > -1)
+          [...parents].map(p => p.plural).indexOf(key.replace('Id', '').plural) === -1)
+          // key !== 'servicii'
 
         if (childrenKeys.length) {
           children.push($t.form.plural)
