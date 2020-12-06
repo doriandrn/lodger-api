26324c13a821533fe6215d276bf9ddf95b548992
Dorian Tudorache <dorian.snaz@gmail.com>
2020-12-06 10:49:24 +0200

    test merged states

--- src/lib/Taxonomy/Subscribable.ts
+++ src/lib/Taxonomy/Subscribable.ts
@@ -95,7 +95,7 @@ implements SubscribableTaxonomy<T> {
     const descriptor = `${subscriberName}-${taxonomies.indexOf(plural)}`
     const subState = state.subs[descriptor] ||
       Object.assign(state.subs, { [descriptor]: {} }) && state.subs[descriptor]
-    const sub = this.subscribers[subscriberName] = new Subscriber(this.collection, options || subState)
+    const sub = this.subscribers[subscriberName] = new Subscriber(this.collection, merge(options, subState))
 
     if (this.parents && this.parents.length && !sub.refsIds) {
       sub.refsIds = observable({})
