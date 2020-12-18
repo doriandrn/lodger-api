207f107211d151af13535e2c6d47ac8d1b787382
Dorian Tudorache <dorian.snaz@gmail.com>
2020-12-18 08:00:18 +0200

    test hooks

--- src/lib/Field.ts
+++ src/lib/Field.ts
@@ -265,7 +265,7 @@ export class Field implements FieldAPI {
    */
   get rxSchema (): RxJsonSchemaTopLevel {
     const schema = {}
-    const excludes = ['storage', 'value', 'v', 'default']
+    const excludes = ['storage', 'value', 'v']
     Object.keys(this).forEach(prop => {
       if (this[prop] === undefined) return
       if (excludes.indexOf(prop) > -1) return
